3ef0a2b5d06d0668a9b65c8f23f8cacf0fdde13f
jdgasser-hmtpro <jdgasser@gmail.com>
2026-01-05 13:23:30 +0100

    Comment out disk commands in init.sh
    Comment out disk and logging commands in init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DISKS=`/sbin/parted -l | grep /dev | grep -v mapper | awk '{print $2;}'| tr '\n' ' ' | sed -e 's/://'`
-echo "#!/bin/bash" > /opt/mrtg/cron1m.sh 
-echo "iostat -x $DISKS 4 12 >> /var/log/iostat.log" >> /opt/mrtg/cron1m.sh
+#DISKS=`/sbin/parted -l | grep /dev | grep -v mapper | awk '{print $2;}'| tr '\n' ' ' | sed -e 's/://'`
+#echo "#!/bin/bash" > /opt/mrtg/cron1m.sh 
+#echo "iostat -x $DISKS 4 12 >> /var/log/iostat.log" >> /opt/mrtg/cron1m.sh
 
 bash /opt/mrtg/update.sh
 
